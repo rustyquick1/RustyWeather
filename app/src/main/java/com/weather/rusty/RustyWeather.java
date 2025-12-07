b39a0f5ee8b1f46dd774858efbd08a11bf56fc93
Winterspark <winterspark@WintersparksAir.lan>
2025-12-07 01:55:50 -0600

    added build.gradle and .gitignore.

---
+++ app/src/main/java/com/weather/rusty/RustyWeather.java
@@ -0,0 +1,12 @@
+package com.weather.rusty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustyWeather {
+  public static void main(String[] args) {
+    SpringApplication.run(RustyWeather.class, args);
+  }
+}
+
